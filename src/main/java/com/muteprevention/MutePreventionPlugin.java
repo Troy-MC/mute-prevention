5a2e255923b54d65f04380cfe20fc79ed868aeb6
Troy_MC <troy.mcdonald85@gmail.com>
2023-03-01 17:47:01 +1000

    Initial changes to create new a Runelite plugin from the template.

--- src/main/java/com/muteprevention/MutePreventionPlugin.java
+++ src/main/java/com/muteprevention/MutePreventionPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.muteprevention;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Mute Prevention"
 )
-public class ExamplePlugin extends Plugin
+public class MutePreventionPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private MutePreventionConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	MutePreventionConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(MutePreventionConfig.class);
 	}
 }
--- src/test/java/com/muteprevention/MutePreventionPluginTest.java
+++ src/test/java/com/muteprevention/MutePreventionPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.muteprevention;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class MutePreventionPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(MutePreventionPlugin.class);
 		RuneLite.main(args);
 	}
 }
